04b0a10658039fc6f323cfc5ce0e59e11e6d3385
AniketGujar <gujaraniket7875@gmail.com>
2020-12-31 14:55:59 +0530

    Added:  Code to validate Last Name

--- UserRegisration.java
+++ UserRegisration.java
@@ -5,7 +5,7 @@ public class UserRegisration {
 	Scanner Scan = new Scanner(System.in);
     
 	public void firstName() {
-		System.out.println("Enter User's First Name:(First Letter Capital and minimum 3 characters)");
+		System.out.println("Enter First Name:(First Letter Capital and minimum 3 characters)");
 		String fName = Scan.nextLine();
 		
 		Pattern fNamePat = Pattern.compile("^[A-Z]+[A-Za-z]{2,12}$");
@@ -21,9 +21,27 @@ public class UserRegisration {
 	    }
 	}
 	
+	public void lastName() {
+		System.out.println("Enter Last Name:(First Letter Capital and minimum 3 characters)");
+		String lName = Scan.nextLine();
+		
+		Pattern lNamePat = Pattern.compile("^[A-Z]+[A-Za-z]{2,12}$");
+	    Matcher lNameMatch = lNamePat.matcher(lName);
+	    boolean lNameverification = lNameMatch.find();
+	    
+	    if (lNameverification==false) {
+	    	System.out.println("Invalid Last Name!! Enter a Valid Last Name");
+	    	lastName();
+	    }
+	    else {
+	    	System.out.println("Valid Last Name: "+ lName);
+	    }
+	}
+	
 	public static void main(String[] args) {
 		UserRegisration user = new UserRegisration();
 		user.firstName();
+		user.lastName();
 	}
 
 }
