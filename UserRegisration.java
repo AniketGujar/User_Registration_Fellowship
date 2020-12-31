cba1322cb36e3315345e44030131e991745fe2e8
AniketGujar <gujaraniket7875@gmail.com>
2020-12-31 18:48:43 +0530

    Added: Code to Validate Password

--- UserRegisration.java
+++ UserRegisration.java
@@ -8,7 +8,7 @@ public class UserRegisration {
 	    System.out.println("Enter First Name:(First Letter Capital and minimum 3 characters)");
 	    String fName = Scan.nextLine();
 		
-	    Pattern fNamePat = Pattern.compile("^[A-Z]+[A-Za-z]{2,12}$");
+	    Pattern fNamePat = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,12}$");
 	    Matcher fNameMatch = fNamePat.matcher(fName);
 	    boolean fNameverification = fNameMatch.find();
 	    
@@ -22,10 +22,10 @@ public class UserRegisration {
 	}
 	
 	public void lastName() {
-		System.out.println("Enter Last Name:(First Letter Capital and minimum 3 characters)");
-		String lName = Scan.nextLine();
+	    System.out.println("Enter Last Name:(First Letter Capital and minimum 3 characters)");
+	    String lName = Scan.nextLine();
 		
-		Pattern lNamePat = Pattern.compile("^[A-Z]+[A-Za-z]{2,12}$");
+	    Pattern lNamePat = Pattern.compile("^[A-Z]+[A-Za-z]{2,12}$");
 	    Matcher lNameMatch = lNamePat.matcher(lName);
 	    boolean lNameverification = lNameMatch.find();
 	    
@@ -39,10 +39,10 @@ public class UserRegisration {
 	}
 	
 	public void email() {
-		System.out.println("Enter Email Id:");
-		String email = Scan.nextLine();
+	    System.out.println("Enter Email Id:");
+	    String email = Scan.nextLine();
 		
-		Pattern emailPat = Pattern.compile("^([a-z]+[0-9a-z-!#$%+^&*_.]*){3,15}@[a-z]{3,8}[.]*([a-z]{2,4})*.[a-z]{2,4}");
+	    Pattern emailPat = Pattern.compile("^([a-z]+[0-9a-z-!#$%+^&*_.]*){3,15}@[a-z]{3,8}[.]*([a-z]{2,4})*.[a-z]{2,4}");
 	    Matcher emailMatch = emailPat.matcher(email);
 	    boolean emailVerification = emailMatch.find();
 	    
@@ -56,10 +56,10 @@ public class UserRegisration {
 	}
 	
 	public void mobileNumber() {
-		System.out.println("Enter Mobile Number:(Country Code<space>Mobile Number)");
-		String number = Scan.nextLine();
+	    System.out.println("Enter Mobile Number:(Country Code<space>Mobile Number)");
+	    String number = Scan.nextLine();
 		
-		Pattern numberPattern = Pattern.compile("^([+]*[1-9]{1}[-]*)[-0-9]{1,4}[\" \"]([1-9]?[0-9]{9})$");
+	    Pattern numberPattern = Pattern.compile("^([+]*[1-9]{1}[-]*)[-0-9]{1,4}[\" \"]([1-9]?[0-9]{9})$");
 	    Matcher numberMatch = numberPattern.matcher(number);
 	    boolean numberVerification = numberMatch.find();
 	    
@@ -72,11 +72,29 @@ public class UserRegisration {
 	    }
 	}
 	
+	public void password() {
+	    System.out.println("Enter Password:(One Letter Capital and minimum 8 characters)");
+	    String password = Scan.nextLine();
+		
+	    Pattern passwordPat = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
+	    Matcher passwordMatch = passwordPat.matcher(password);
+	    boolean passwordVerification = passwordMatch.find();
+	    
+	    if (passwordVerification==false) {
+	    	System.out.println("Invalid Password!! Enter a Valid Password");
+	    	password();
+	    }
+	    else {
+	    	System.out.println("Valid Password: **********");
+	    }
+	}
+	
 	public static void main(String[] args) {
 		UserRegisration user = new UserRegisration();
 		user.firstName();
 		user.lastName();
 		user.email();
 		user.mobileNumber();
+		user.password();
 	}
 }
